00bebb941703b77edb8fccc2d3e7c24aa390c683
Jason McVetta <jason.mcvetta@gmail.com>
2022-09-23 22:15:31 -0400

    test path has /bin

--- lib/utils.bash
+++ lib/utils.bash
@@ -100,7 +100,7 @@ install_version() {
     # TODO: Assert gitleaks executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
